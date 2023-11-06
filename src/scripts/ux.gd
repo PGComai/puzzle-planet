c33a5822db60ea5ec1632c7380d011e48c30be92
PGComai <comai.alex@gmail.com>
2023-11-06 11:31:49 -0800

    tablet mode fix

--- src/scripts/ux.gd
+++ src/scripts/ux.gd
@@ -4,6 +4,11 @@ var global
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	global = get_node('/root/Global')
+	var ar = size.x / size.y
+	if ar >= 0.6 and not global.tablet_mode:
+		global.tablet_mode = true
+	elif ar < 0.6 and global.tablet_mode:
+		global.tablet_mode = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
